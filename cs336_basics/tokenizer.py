d440bbc90497ee42e15571d54db1924534814706
Adir Katz <adir.katz@cytoreason.com>
2025-10-15 11:04:01 +0300

    completre skeleton of the tokenizer

--- cs336_basics/tokenizer.py
+++ cs336_basics/tokenizer.py
@@ -1,4 +1,7 @@
 
+from collections.abc import Iterable
+
+
 class Tokenizer:
 
     def __init__(self, vocab : dict[int, bytes], merges : list[tuple[bytes, bytes]], special_tokens : list[str] | None = None):
@@ -6,4 +9,14 @@ class Tokenizer:
         self.merges = merges
         self.special_tokens = special_tokens
 
-    
+    def from_files(cls, vocab_filepath : str, merges_filepath : str, special_tokens : list[str] | None = None):
+        pass
+
+    def encode(self, text: str) -> list[int]:
+        pass
+
+    def encode_iterable(self, iterable: Iterable[int]) -> Iterable[int]:
+        pass
+
+    def decode(self, ids: list[int]) -> str:
+        pass
