5e445481d75adb5d01a1161072e7cd379f7d9d9a
Adir Katz <adir.katz@cytoreason.com>
2025-10-16 16:11:14 +0300

    add tokenize script

---
+++ cs336_basics/scripts/tokenize.py
@@ -0,0 +1,32 @@
+from typing import Iterable
+import numpy as np
+import time
+from tqdm import tqdm
+
+from cs336_basics.tokenizer import Tokenizer
+
+owt_conf = {
+    'train':'data/raw/owt_train.txt',
+    'val':'data/raw/owt_valid.txt',
+    'vocab_filepath': 'data/out/owt_vocab.json',
+    'merges_filepath': 'data/out/owt_merges.txt',
+    'special_tokens': ['<|endoftext|>']
+}
+
+tokenizer = Tokenizer.from_files(**owt_conf)
+
+for split in ['train', 'val']:
+    with open(owt_conf[split]) as f:
+        text = f.read()
+    encoded = tokenizer.encode(text)
+
+    # save the ids
+    total_batches = 1024
+    batch_size = len(encoded) // total_batches
+    arr = np.memmap(f'data/owt/{split}.bin', dtype=np.uint16, mode='w+', shape=(len(encoded),))
+    idx = 0
+    for batch_idx in tqdm(range(total_batches), desc=f'Writing {split}.bin'):
+        batch = encoded[idx:idx+batch_size]
+        arr[idx:idx+batch_size] = batch
+        idx += batch_size
+arr.flush()
